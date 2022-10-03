8c9d00a3cf67052358554660b1765d23450221e4
BoT143 <you@example.com>
2022-10-03 10:45:38 +0300

    First task done

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Imput first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine()); //9
+Console.Write("Imput second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine()); //3
+
+if(n1 > n2)
+{
+    Console.WriteLine("Max number: " + n1 + " Min number: " + n2);
+}
+else
+{
+    Console.WriteLine("Max number: " + n2 + " Min number: " + n1);
+}
+*/
