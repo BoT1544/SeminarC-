c97d63d33489e52e84e5c995bff44c7e63b5b508
BoT143 <you@example.com>
2022-10-03 11:08:10 +0300

    Second task done

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,9 +1,9 @@
-﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Задача 1: напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 /*
 Console.Write("Imput first number: ");
-int n1 = Convert.ToInt32(Console.ReadLine()); //9
+int n1 = Convert.ToInt32(Console.ReadLine()); 
 Console.Write("Imput second number: ");
-int n2 = Convert.ToInt32(Console.ReadLine()); //3
+int n2 = Convert.ToInt32(Console.ReadLine()); 
 
 if(n1 > n2)
 {
@@ -12,5 +12,39 @@ if(n1 > n2)
 else
 {
     Console.WriteLine("Max number: " + n2 + " Min number: " + n1);
+}
+*/
+
+
+// Задача 2: напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("Imput first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput three number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 > n2)
+{
+    if(n1 > n3)
+    {
+        Console.WriteLine("Max number: " + n1);
+    }
+    else
+    {
+        Console.WriteLine("Max number: " + n3);
+    }
+}
+else
+{
+    if(n2 > n3)
+    {
+        Console.WriteLine("Max number: " + n2);
+    }
+    else
+    {
+        Console.WriteLine("Max number: " + n3);
+    }
 }
 */
